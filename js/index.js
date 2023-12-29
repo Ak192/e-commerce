8abdc2d8736354cb754d5cc3da5b97b13b25b365
vikash kumar <84111117+Ak192@users.noreply.github.com>
2023-12-29 19:51:49 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/* include Header & footer  page */
+
+$(function () {
+    $('#header').load('comman/header.html');
+    $('#footer').load('comman/footer.html');
+})
+
+ /* slick slider  for slide the feature products */
+$('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    
+    ]
+});
